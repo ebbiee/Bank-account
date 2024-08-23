89d9c7eaae8adb70adde59d737a6f44319bdeed9
Fabunmi Ebenezer <fabunmiebenezer37@gmail.com>
2024-08-23 02:04:39 +0100

    add the function for adding money  and logging in

--- js/account.js
+++ js/account.js
@@ -1,168 +1,210 @@
-class User {
-    constructor(accountNumber, username, initialBalance = 0.00) {
-      this.accountNumber = accountNumber;
-      this.username = username;
-      this.balance = initialBalance;
-      this.transactions = [];
-    }
-  
-    // Method to add money to the user's account
-    addMoney(amount) {
-      if (amount > 0) {
-        this.balance += amount;
-        return true;
-      }
-      return false;
-    }
-  
-    // Method to transfer money to another account
-    transferMoney(amount, recipient) {
-      if (amount > 0 && amount <= this.balance) {
-        this.balance -= amount;
-        this.transactions.push({ recipient: recipient.accountNumber, amount: amount });
-        recipient.receiveMoney(amount, this.accountNumber);
-        return true;
-      }
-      return false;
-    }
-  
-    // Method to receive money from another account
-    receiveMoney(amount, senderAccountNumber) {
-      this.balance += amount;
-      this.transactions.push({ sender: senderAccountNumber, amount: amount });
-    }
-  
-    // Method to get the transaction history
-    getTransactionHistory() {
-      return this.transactions;
-    }
-  }
-  
-  const userManager = (() => {
-    const users = {};
-  
-    // Function to create a new user
-    const createUser = (accountNumber, username, initialBalance = 0.00) => {
-      if (!users[accountNumber]) {
-        users[accountNumber] = new User(accountNumber, username, initialBalance);
-        return users[accountNumber];
-      }
-      return null; // User already exists
-    };
-  
-    // Function to get a user by account number
-    const getUser = (accountNumber) => {
-      return users[accountNumber] || null;
-    };
-  
-    return {
-      createUser,
-      getUser,
-    };
-  })();
-  $(document).ready(function() {
-    // Initial Users for Demo
-    userManager.createUser('2003685', 'Fabunmi Ebenezer Oluwarotimi', 0.00);
-    userManager.createUser('2003686', 'John Doe', 1000.00);
-  
-    // Login functionality
-    $('#login-form').on('submit', function(e) {
-      e.preventDefault();
-      const accountNumber = $('#login-username').val();
-      const user = userManager.getUser(accountNumber);
-      
-      if (user) {
-        updateUIForLoggedInUser(user);
-        alert('Login successful!');
-      } else {
-        alert('Account not found.');
-      }
-    });
-  
-    // Show Transaction History
-    $('.history').on('click', function() {
-      const accountNumber = $('.accountnumber').text().trim();
-      const user = userManager.getUser(accountNumber);
-      
-      if (user) {
-        displayTransactionHistory(user.getTransactionHistory());
-      } else {
-        alert('No user logged in.');
-      }
-    });
-  
-    // Add Money functionality
-    $('.add-money').on('click', function() {
-      $('#offcanvasTop').on('submit', 'form', function(e) {
-        e.preventDefault();
-        const accountNumber = $('.accountnumber').text().trim();
-        const user = userManager.getUser(accountNumber);
-        const amountToAdd = parseFloat($(this).find('input[type="number"]').val());
-        
-        if (user && user.addMoney(amountToAdd)) {
-          updateBalanceUI(user);
-          alert('Money added successfully!');
-          $(this).find('input[type="number"]').val('');
-        } else {
-          alert('Invalid amount or account not found.');
-        }
-      });
-    });
-  
-    // Transfer Money functionality
-    $('.openModalBtn').on('click', function() {
-      $('#myModal').show();
-    });
-  
-    $('.close').on('click', function() {
-      $('#myModal').hide();
-    });
-  
-    $('#transferForm').on('submit', function(e) {
-      e.preventDefault();
-      const accountNumber = $('.accountnumber').text().trim();
-      const recipientAccountNumber = $(this).find('input[type="text"]').val().trim();
-      const amountToTransfer = parseFloat($(this).find('input[type="number"]').val());
-      const user = userManager.getUser(accountNumber);
-      const recipient = userManager.getUser(recipientAccountNumber);
-      
-      if (user && recipient && user.transferMoney(amountToTransfer, recipient)) {
-        updateBalanceUI(user);
-        alert('Transfer successful!');
-        $(this).find('input').val('');
-        $('#myModal').hide();
-      } else {
-        alert('Invalid transfer amount, insufficient funds, or recipient not found.');
-      }
-    });
-  
-    // Helper function to update the UI after a successful login
-    function updateUIForLoggedInUser(user) {
-      $('.account-holder').text(user.username);
-      $('.money').text(user.balance.toFixed(2));
-      $('.accountnumber').text(user.accountNumber);
-    }
-  
-    // Helper function to update the balance UI
-    function updateBalanceUI(user) {
-      $('.money').text(user.balance.toFixed(2));
-    }
-  
-    // Helper function to display transaction history
-    function displayTransactionHistory(transactions) {
-      if (transactions.length > 0) {
-        let transactionHTML = '';
-        transactions.forEach((transaction) => {
-          if (transaction.recipient) {
-            transactionHTML += `<p>Transferred &#8358; ${transaction.amount} to ${transaction.recipient}</p>`;
-          } else if (transaction.sender) {
-            transactionHTML += `<p>Received &#8358; ${transaction.amount} from ${transaction.sender}</p>`;
-          }
-        });
-        $('.offcanvas-body').html(transactionHTML);
-      } else {
-        $('.offcanvas-body').html('<p>No transactions found.</p>');
-      }
-    }
-  });
-      
+// // Business logic
+// function Bank() {
+//   this.accounts = {};
+//   this.currentId = 2209146835;
+// }
+
+// Bank.prototype.assignID = function() {
+//   this.currentId += 1268;
+//   return this.currentId;
+// };
+
+// Bank.prototype.addAccount = function(account) {
+//   account.id = this.assignID();
+//   this.accounts[account.id] = account;
+// };
+
+// Bank.prototype.deleteAccount = function(id) {
+//   if (this.accounts[id] === undefined) {
+//     return false;
+//   }
+//   delete this.accounts[id];
+//   return true;
+// };
+
+// Bank.prototype.findAccount = function(id) {
+//   if (this.accounts[id] != undefined) {
+//     return this.accounts[id];
+//   }
+//   return false;
+// };
+
+// function AccountHolder(username, password, emailAddress) {
+//   this.username = username;
+//   this.password = password;
+//   this.emailAddress = emailAddress;
+//   this.balance = 0;
+//   this.transactionHistory = [];
+// }
+
+// AccountHolder.prototype.getUsername = function() {
+//   return this.username;
+// };
+
+// AccountHolder.prototype.getPassword = function() {
+//   return this.password;
+// };
+
+// AccountHolder.prototype.getEmailAddress = function() {
+//   return this.emailAddress;
+// };
+
+// AccountHolder.prototype.getBalance = function() {
+//   return this.balance;
+// };
+
+// AccountHolder.prototype.addTransaction = function(amount, accountNumber) {
+//   this.transactionHistory.push(new TransactionHistory(amount, accountNumber));
+// };
+
+// function TransactionHistory(amount, accountNumber) {
+//   this.amount = amount;
+//   this.accountNumber = accountNumber;
+// }
+
+// // User interface
+// $(document).ready(function() {
+//   const bank = new Bank();
+
+//   $("form").submit(function(event) {
+//     event.preventDefault();
+//     loginAndSignUp($(this));
+//   });
+
+//   function loginAndSignUp(form) {
+//     const formId = form.attr("id");
+//     if (formId === "signup-form") {
+//       const username = $("#signup-username").val();
+//       const password = $("#signup-password").val();
+//       const email = $("#exampleFormControlInput1").val();
+
+//       const newAccount = new AccountHolder(username, password, email);
+//       bank.addAccount(newAccount);
+//       alert("Account created successfully!");
+//       form[0].reset();
+//     } else if (formId === "login-form") {
+//       const accountNumber = $("#account-number").val();
+//       const password = $("#login-password").val();
+
+//       const account = bank.findAccount(accountNumber);
+//       if (account && account.getPassword() === password) {
+//         alert("Login successful!");
+//         // Display account dashboard
+//         displayAccountDashboard(account);
+//       } else {
+//         alert("Invalid account number or password");
+//       }
+//     }
+//   }
+
+//   function displayAccountDashboard(account) {
+//     $(".body2").show();
+//     $(".account-holder").text(account.getUsername());
+//     $(".accountnumber").text(account.id);
+//     $(".money").text(account.getBalance());
+
+//     // Display transaction history
+//     const transactionHistory = account.transactionHistory;
+//     $(".offcanvas-body").html("");
+//     transactionHistory.forEach((transaction) => {
+//       const html = `
+//         <p>
+//           Transferred <span class="price">&#8358; ${transaction.amount} to </span
+//           ><span class="account-number">${transaction.accountNumber}</span>
+//         </p>
+//       `;
+//       $(".offcanvas-body").append(html);
+//     });
+//   }
+
+//   const loginBtn = $("#login-btn");
+//   const signupBtn = $("#signup-btn");
+//   const loginForm = $("#login-form");
+//   const signupForm = $("#signup-form");
+
+//   loginBtn.click(function() {
+//     loginForm.addClass("active");
+//     signupForm.removeClass("active");
+//     loginBtn.addClass("active");
+//     signupBtn.removeClass("active");
+//   });
+
+//   signupBtn.click(function() {
+//     signupForm.addClass("active");
+//     loginForm.removeClass("active");
+//     signupBtn.addClass("active");
+//     loginBtn.removeClass("active");
+//   });
+
+//   const modal = $("#myModal");
+//   $(".openModalBtn").on("click", function() {
+//     modal.fadeIn();
+//   });
+
+//   $(".close").on("click", function() {
+//     modal.fadeOut();
+//   });
+
+//   $(window).on("click", function(event) {
+//     if ($(event.target).is(modal)) {
+//       modal.hide();
+//     }
+//   });
+
+//   // Transfer money functionality
+//   $("#transferForm").submit(function(event) {
+//     event.preventDefault();
+//     const recipientAccountNumber = $("#recipient-account-number").val();
+//     const amount = $("#transfer-amount").val();
+
+//     const account = bank.findAccount(recipientAccountNumber);
+//     if (account) {
+//       const currentAccount = bank.findAccount($(".account-holder").text());
+//       if (currentAccount.getBalance() >= amount) {
+//         currentAccount.addTransaction(-amount, recipientAccountNumber);
+//         account.addTransaction(amount, currentAccount.id);
+//         alert("Transfer successful!");
+//         displayAccountDashboard(currentAccount);
+//       } else {
+//         alert("Insufficient balance");
+//       }
+//     } else {
+//       alert("Recipient account not found");
+//     }
+//   });
+
+// // Add money functionality
+// $("#add-money-form").submit(function(event) {
+//   event.preventDefault();
+//   const amount = $("#add-money-amount").val();
+
+//   const currentAccount = bank.findAccount($(".account-holder").text());
+//   if (currentAccount) {
+//     currentAccount.addTransaction(amount, "Deposit");
+//     alert("Money added successfully!");
+//     displayAccountDashboard(currentAccount);
+//   } else {
+//     alert("Error adding money");
+//   }
+// });
+
+// // Display account dashboard
+// function displayAccountDashboard(account) {
+//   $(".body2").show();
+//   $(".account-holder").text(account.getUsername());
+//   $(".accountnumber").text(account.id);
+//   $(".money").text(account.getBalance());
+
+//   // Display transaction history
+//   const transactionHistory = account.transactionHistory;
+//   $(".offcanvas-body").html("");
+//   transactionHistory.forEach((transaction) => {
+//     const html = `
+//       <p>
+//         ${transaction.amount > 0 ? "Deposited" : "Transferred"} <span class="price">&#8358; ${Math.abs(transaction.amount)} ${transaction.accountNumber ? "to " + transaction.accountNumber : ""}</span>
+//       </p>
+//     `;
+//     $(".offcanvas-body").append(html);
+//   });
+// }
+// });
--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 //business logic
 function Bank() {
     this.accounts = {};
-    this.currentId = 2209146835;
+    this.currentId = 220914683;
   }
   Bank.prototype.assignID = function () {
     this.currentId += 1268;
@@ -24,6 +24,35 @@ function Bank() {
     }
     return false;
   };
+  function accountHolder(username,password,emailAddress){
+    this.username = username;
+    this.password = password;
+    this.emailAddress = emailAddress;
+    this.balance = 0;
+    this.transactionHistory = [];
+  }
+  function transactionHistory(amount,  accountNumber){
+    this.amount = amount;
+    this.accountNumber = accountNumber; 
+  }
+  accountHolder.prototype.username = function(){
+    return this.username;
+  }
+  accountHolder.prototype.getPassword = function() {
+    return this.password;
+  };
+  accountHolder.prototype.getBalance = function() {
+    return this.balance;
+  };
+  accountHolder.prototype.addTransaction = function(amount, accountNumber) {
+    this.transactionHistory.push(new TransactionHistory(amount, accountNumber));
+  };
+  accountHolder.prototype.depositMoney = function (amount) {
+    let initial = parseInt(this.balance);
+    let deposit = parseInt(amount);
+    this.balance = deposit + initial;
+    return this.balance;
+}
 
 
 
@@ -34,6 +63,68 @@ function Bank() {
 
 //user iterface
 $(document).ready(function(){
+let bank = new Bank();
+let loggedInAccount = null;
+
+$("form").submit(function (event) {
+  event.preventDefault();
+  const formId = $(this).attr("id");
+
+  if (formId === "signup-form") {
+    const username = $("#signup-username").val();
+    const password = $("#signup-password").val();
+    const email = $("#exampleFormControlInput1").val();
+
+    // Create a new account and add it to the bank
+    const newAccount = new accountHolder(username, password, email);
+    bank.addAccount(newAccount);
+
+    // Display the username and account number on the screen
+    $(".account-holder").html(username);
+    $(".accountnumber").html(newAccount.id); // Display the account number
+
+    console.log("Account created successfully! Your Account Number is " + newAccount.id);
+    $(this)[0].reset();
+  } 
+  else if (formId === "login-form") {
+    const accountNumber = $("#account-number").val();
+    const password = $("#login-password").val();
+    console.log(accountNumber, password)
+
+    // Find the account by ID
+    const account = bank.findAccount(accountNumber);
+    console.log(account)
+
+    if (account && account.password === password) {
+      loggedInAccount = account;
+      alert("Login successful!");
+      showAccountDetails(loggedInAccount);
+    } else {
+      alert("Login failed. Please check your account number and password.");
+    }
+  }
+  if (formId === "addMoneyForm") {
+    const amount = parseInt($(".amountToAdd").val());
+    console.log('Logged in account during add money:', loggedInAccount);
+    if (loggedInAccount && typeof loggedInAccount.depositMoney === "function") {
+      let result = loggedInAccount.depositMoney(amount);
+      $(".money").html(result); // Update the displayed balance
+      console.log('Deposited amount:', amount, 'New balance:', result);
+    } else {
+      console.log("No account is logged in or depositMoney method is missing");
+    }
+    $(this)[0].reset();
+  }
+});
+
+function showAccountDetails(account) {
+  $(".account-holder").html(account.username);
+  $(".accountnumber").html(account.id);
+  $(".money").html(account.balance.toFixed(2));
+  // Additional account details can be displayed here
+}
+
+
 const loginBtn = $('#login-btn');
 const signupBtn =$('#signup-btn');
 const loginForm = $('#login-form');
@@ -60,7 +151,7 @@ $(".close").on("click", function() {
     modal.fadeOut();
 });
 $(window).on("click", function(event) {
-    if ($(event.target).is($modal)) {
+    if ($(event.target).is(modal)) {
         modal.hide();
     }
 });
